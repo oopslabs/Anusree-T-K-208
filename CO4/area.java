a14536b9d528e5d6387184e310f6fa76e4d05cfe
Anusree T K <94076173+anusreeanilanu@users.noreply.github.com>
2022-06-09 11:57:25 +0530

    QS1

---
+++ CO4/area.java
@@ -0,0 +1,28 @@
+package oop;
+import java.util.*;
+import graphics.shapes;
+public class area {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length and breadth of a rectangle: ");
+		int l=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("Enter the side  of a square: ");
+		int a=sc.nextInt();
+		System.out.println("Enter the base and height of a triangle: ");
+		float ba=sc.nextFloat();
+		float h=sc.nextFloat();
+		System.out.println("Enter the radius of a circle: ");
+	    float r=sc.nextFloat();
+	    shapes s=new shapes();
+	    s.rectangle(l,b);
+	    s.square(a);;
+	    s.triangle(ba, h);;
+	    s.circle(r);
+		
+		
+	}
+	
+
+}
