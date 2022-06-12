059a7c6545585212852129687ab18a42dac2c336
Anusree T K <94076173+anusreeanilanu@users.noreply.github.com>
2022-06-13 01:06:22 +0530

    Create stackremovel.java

---
+++ CO4/QS12/stackremovel.java
@@ -0,0 +1,25 @@
+package new123;
+import java.util.*;
+public class stackremovel {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		Stack<String>sta=new Stack<>();
+		System.out.println("Enter the limit:");
+		int n=sc.nextInt();
+		String str;
+		sc.nextLine();
+		System.out.println("Enter a String:");
+		for(int i=0;i<n;i++)
+		{
+			str=sc.nextLine();
+			sta.push(str);
+		}
+		System.out.println("The data are:"+sta);
+		System.out.println("Enter the index to remove an element from stack");
+		int in=-sc.nextInt();
+		sta.remove(in);
+		System.out.println("After removel the stack is:"+sta);
+		
+	}
+
+}
